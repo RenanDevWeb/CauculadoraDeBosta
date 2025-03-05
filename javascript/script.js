3ba1170e1b3bc9690bc1fe69d7a9052d14bea857
Renan <renansalmazio@gmail.com>
2025-03-05 20:46:20 -0300

    arquivos do projeto

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,39 @@
-const quantidadeDeRolos = document.querySelector("#qtdederolos");
-const precototalpacote = document.querySelector("#precopacote");
-const metragemporrolo = document.querySelector("#metrosporrolo");
+const quantidaDeDeRolos = document.querySelector("#qtdederolos");
+const precoTotalPacote = document.querySelector("#precopacote");
+const metragemPorRolo = document.querySelector("#metrosporrolo");
 const caculadoraDeBosta = document.querySelector("#caculadoraDeBosta");
-const roloText =   document.querySelector("#rolo")
-const metragemText  =  document.querySelector("#metragem")
-let metragemporroloValor;
+const roloText = document.querySelector("#rolo");
+const metragemText = document.querySelector("#metragem");
+const infos = document.querySelector(".infos");
+let metragemPorRoloValor;
 
 caculadoraDeBosta.addEventListener("submit", function (e) {
-    e.preventDefault()
-    let quantidadeDeRolosValor = quantidadeDeRolos.value
-    let precototalpacoteValor  =  precototalpacote.value
-    metragemporroloValor =  metragemporrolo.value
-    precoporrolo(precototalpacoteValor,quantidadeDeRolosValor)
-    
+  e.preventDefault();
+  let quantidaDeDeRolosValor = quantidaDeDeRolos.value;
+  let precoTotalPacoteValor = precoTotalPacote.value;
+  metragemPorRoloValor = metragemPorRolo.value;
+
+  if (quantidaDeDeRolosValor !== "" && precoTotalPacoteValor !== "" && metragemPorRoloValor !== "") {
+    infos.classList.remove("infos");
+    infos.classList.add("infos-show");
+    precoporrolo(precoTotalPacoteValor, quantidaDeDeRolosValor);
+  }else{
+    Swal.fire("Digite um valor valido");
+  }
 
 });
 
-function precoporrolo(precototalpacote,quantidadeDeRolos) {
-  var soma = precototalpacote / quantidadeDeRolos;
-  var valorarredondado = Math.ceil(soma * 100) / 100
-  roloText.innerText = valorarredondado.toFixed(2)
-  precometrorolo(valorarredondado)
+function precoporrolo(precoTotalPacote, quantidadeDeRolos) {
+  var soma = precoTotalPacote / quantidadeDeRolos;
+  var valorarredondado = Math.ceil(soma * 100) / 100;
+  roloText.innerText = valorarredondado.toFixed(2);
+  precometrorolo(valorarredondado);
 }
 
 function precometrorolo(valorarredondado) {
-  var soma1 = valorarredondado / metragemporroloValor;
-  metragemText.innerText = soma1
+  var soma1 = valorarredondado / metragemPorRoloValor;
+  metragemText.innerText = soma1;
 }
 
-// var precoporrolo =  precototalpacote /quantidadeDeRolos
+// var precoporrolo =  precoTotalPacote /quantidadeDeRolos
 // var precometrorolo = precoporrolo / metragemporrolo
